7e960b243e676541c13cebcc115f0598d359d58b
Reykira007 <rearryc@gmail.com>
2023-12-25 00:18:51 +0700

    setup templates sb admin2

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['title'] = "Dashboard";
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/dashboard', $data);
+        $this->load->view('templates_admin/footer');
+    }
+}
+
+/* End of file Dashboard.php and path \application\controllers\admin\Dashboard.php */
