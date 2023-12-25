7a83082315184ce4576dc11851d0435257e57409
Reykira007 <rearryc@gmail.com>
2023-12-26 04:08:16 +0700

    add delete&update data pegawai

--- application/controllers/admin/DataPegawai.php
+++ application/controllers/admin/DataPegawai.php
@@ -45,17 +45,7 @@ class DataPegawai extends CI_Controller
             $status = $this->input->post('status');
             $photo = $_FILES['photo']['name'];
 
-            // Gunakan operator kesamaan di sini
             if ($photo == '') {
-                $this->session->set_flashdata('pesan', '<div class="alert 
-                alert-danger alert-dismissible fade show" role="alert">
-                <strong>Foto wajib diisi!</strong>
-                <button type="button" class="close" data-dismiss="alert" 
-                aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-                </div>');
-                redirect('admin/dataPegawai/tambahData');
             } else {
                 $config['upload_path'] = './assets/photo';
                 $config['allowed_types'] = 'jpg|jpeg|png|tiff';
@@ -76,7 +66,6 @@ class DataPegawai extends CI_Controller
                 'status' => $status,
                 'photo' => $photo,
             );
-
             $this->penggajianModel->insert_data($data, 'data_pegawai');
             $this->session->set_flashdata('pesan', '<div class="alert 
             alert-success alert-dismissible fade show" role="alert">
@@ -90,6 +79,86 @@ class DataPegawai extends CI_Controller
         }
     }
 
+    public function updateData($id)
+    {
+        $where = array('id_pegawai' => $id);
+        $data['title'] = 'Update Data Pegawai';
+        $data['jabatan'] = $this->penggajianModel->get_data('data_jabatan')->result();
+        $data['pegawai'] = $this->db->query("SELECT * FROM data_pegawai WHERE id_pegawai = '$id'")->result();
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/formUpdatePegawai', $data);
+        $this->load->view('templates_admin/footer');
+    }
+
+    public function updateDataAksi()
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->updateData();
+        } else {
+            $id = $this->input->post('id_pegawai');
+            $nik = $this->input->post('nik');
+            $nama_pegawai = $this->input->post('nama_pegawai');
+            $jenis_kelamin = $this->input->post('jenis_kelamin');
+            $jabatan = $this->input->post('jabatan');
+            $tanggal_masuk = $this->input->post('tanggal_masuk');
+            $status = $this->input->post('status');
+            $photo = $_FILES['photo']['name'];
+
+            // Dapatkan nama foto lama
+            $old_photo = $this->db->get_where('data_pegawai', ['id_pegawai' => $id])->row('photo');
+
+            if ($photo) {
+                // Hapus foto lama dari folder
+                $old_photo_path = FCPATH . './assets/photo/' . $old_photo;
+                if (file_exists($old_photo_path)) {
+                    unlink($old_photo_path);
+                }
+
+                // Upload foto baru
+                $config['upload_path'] = './assets/photo';
+                $config['allowed_types'] = 'jpg|jpeg|png|tiff';
+                $this->load->library('upload', $config);
+                if ($this->upload->do_upload('photo')) {
+                    $photo = $this->upload->data('file_name');
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $data = array(
+                'nik' => $nik,
+                'nama_pegawai' => $nama_pegawai,
+                'jenis_kelamin' => $jenis_kelamin,
+                'jabatan' => $jabatan,
+                'tanggal_masuk' => $tanggal_masuk,
+                'status' => $status,
+            );
+
+            // Jika ada foto baru, set data photo ke nama foto baru
+            if ($photo) {
+                $data['photo'] = $photo;
+            }
+
+            $where = array(
+                'id_pegawai' => $id
+            );
+
+            $this->penggajianModel->update_data('data_pegawai', $data, $where);
+            $this->session->set_flashdata('pesan', '<div class="alert 
+        alert-success alert-dismissible fade show" role="alert">
+        <strong>Data berhasil diupdate!</strong>
+        <button type="button" class="close" data-dismiss="alert" 
+        aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>');
+            redirect('admin/dataPegawai');
+        }
+    }
+
     public function _rules()
     {
         $this->form_validation->set_rules('nik', 'nik', 'required');
@@ -98,6 +167,21 @@ class DataPegawai extends CI_Controller
         $this->form_validation->set_rules('jabatan', 'jabatan', 'required');
         $this->form_validation->set_rules('tanggal_masuk', 'tanggal_masuk', 'required');
         $this->form_validation->set_rules('status', 'status', 'required');
+    }
+
+    public function deleteData($id)
+    {
+        $where = array('id_pegawai' => $id);
+        $this->penggajianModel->delete_data($where, 'data_pegawai');
+        $this->session->set_flashdata('pesan', '<div class="alert 
+        alert-danger alert-dismissible fade show" role="alert">
+        <strong>Data berhasil dihapus!</strong>
+        <button type="button" class="close" data-dismiss="alert" 
+        aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>');
+        redirect('admin/dataPegawai');
     }
 }
 
--- application/models/penggajianModel.php
+++ application/models/penggajianModel.php
@@ -7,6 +7,7 @@ class PenggajianModel extends CI_Model
     {
         return $this->db->get($table);
     }
+
     public function insert_data($data, $table)
     {
         $this->db->insert($table, $data);
@@ -14,20 +15,30 @@ class PenggajianModel extends CI_Model
 
     public function update_data($table, $data, $where)
     {
+        // Perbarui data di tabel database
         $this->db->update($table, $data, $where);
     }
 
     public function delete_data($where, $table)
     {
+        // Dapatkan nama file photo sebelum menghapus data
+        $photo_data = $this->db->get_where($table, $where)->row();
+        $photo_filename = $photo_data->photo;
+
+        // Hapus file foto dari folder
+        $photo_path = FCPATH . './assets/photo/' . $photo_filename;
+        if (file_exists($photo_path)) {
+            unlink($photo_path);
+        }
+
         $this->db->where($where);
         $this->db->delete($table);
     }
+
     public function select()
     {
-
+        // Implementasi fungsi select jika diperlukan
     }
-
 }
-
 
 /* End of file PenggajianModel_model.php and path \application\models\models\PenggajianModel_model.php */
