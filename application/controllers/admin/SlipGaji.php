284aafe43ba29bf4bb281f5b8538fcabca9ca0a9
Reykira007 <rearryc@gmail.com>
2024-01-21 16:54:25 +0700

    add slipGaji

---
+++ application/controllers/admin/SlipGaji.php
@@ -0,0 +1,68 @@
+<?php
+
+class SlipGaji extends CI_Controller
+{
+    public function index()
+    {
+        $data['title'] = "Slip Gaji Pegawai";
+        $data['pegawai'] = $this->penggajianModel->get_data('data_pegawai')->result();
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/gaji/slipGaji', $data);
+        $this->load->view('templates_admin/footer');
+    }
+
+    public function cetakSlipGaji()
+    {
+        $data['title'] = "Cetak Slip Gaji Pegawai";
+        $nama = $this->input->post('nama_pegawai');
+        $bulan = $this->input->post('bulan');
+        $tahun = $this->input->post('tahun');
+
+        // Memeriksa apakah nama pegawai, bulan, dan tahun telah dipilih
+        if (empty($nama) || empty($bulan) || empty($tahun)) {
+            $this->session->set_flashdata('error', 'Nama Pegawai, Bulan, dan Tahun harus dipilih.');
+            redirect('admin/slipGaji', 'refresh');
+        }
+
+        $bulantahun = $bulan . $tahun;
+
+        // Mengambil data potongan gaji untuk digunakan di view
+        $data['potongan'] = $this->penggajianModel->getAllPotonganGaji();
+
+        // Memeriksa apakah data kehadiran untuk nama pegawai, bulan, dan tahun yang dipilih tersedia
+        $checkDataQuery = $this->db->query("
+        SELECT COUNT(*) as total_rows
+        FROM data_kehadiran
+        WHERE bulan = '$bulantahun' AND nama_pegawai = '$nama'
+    ");
+
+        $result = $checkDataQuery->row();
+
+        if ($result->total_rows == 0) {
+            $this->session->set_flashdata('error', 'Data kehadiran untuk ' . $nama . ' pada bulan ' . $bulan . ' tahun ' . $tahun . ' tidak ditemukan.');
+            redirect('admin/slipGaji', 'refresh');
+        }
+
+        // Mendapatkan data untuk cetak slip gaji
+        $data['cetak_slip_gaji'] = $this->db->query("
+        SELECT
+            data_pegawai.nik,
+            data_pegawai.nama_pegawai,
+            data_pegawai.jenis_kelamin,
+            data_jabatan.nama_jabatan,
+            data_jabatan.gaji_pokok,
+            data_jabatan.tj_transport,
+            data_jabatan.uang_makan,
+            data_kehadiran.sakit,
+            data_kehadiran.alpa
+        FROM data_pegawai
+        INNER JOIN data_kehadiran ON data_kehadiran.nik = data_pegawai.nik
+        INNER JOIN data_jabatan ON data_jabatan.nama_jabatan = data_pegawai.jabatan
+        WHERE data_kehadiran.bulan = '$bulantahun' AND data_kehadiran.nama_pegawai='$nama'
+    ")->result();
+
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('admin/gaji/cetakSlipGaji', $data);
+    }
+}
---
+++ application/views/admin/gaji/cetakSlipGaji.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title><?php echo $title ?></title>
+    <style type="text/css">
+        body {
+            font-family: Arial;
+            color: black;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>PT. Indonesia Bangkit</h1>
+        <h2>Daftar Gaji Pegawai</h2>
+        <hr style="width: 50%; border-width: 5px; color: black">
+    </center>
+
+    <?php foreach ($potongan as $p) {
+        $potongan = $p->jml_potongan;
+    } ?>
+
+    <?php
+    $bulan = $this->input->post('bulan') ? $this->input->post('bulan') : date('m');
+    $tahun = $this->input->post('tahun') ? $this->input->post('tahun') : date('Y');
+    ?>
+
+    <?php foreach ($cetak_slip_gaji as $ps) :
+        $potongan_sakit = $this->penggajianModel->getPotonganGajiByJenis('sakit')->jml_potongan * $ps->sakit;
+        $potongan_alpa = $this->penggajianModel->getPotonganGajiByJenis('alpa')->jml_potongan * $ps->alpa;
+        $total_potongan = $potongan_sakit + $potongan_alpa;
+        $total_gaji = $ps->gaji_pokok + $ps->tj_transport + $ps->uang_makan - $potongan_sakit - $potongan_alpa;
+    ?>
+        <table style="width: 100%">
+            <tr>
+                <td width="20%">Nama Pegawai</td>
+                <td>:</td>
+                <td><?php echo $ps->nama_pegawai ?></td>
+            </tr>
+            <tr>
+                <td>NIK</td>
+                <td>:</td>
+                <td><?php echo $ps->nik ?></td>
+            </tr>
+            <tr>
+                <td>Jabatan</td>
+                <td>:</td>
+                <td><?php echo $ps->nama_jabatan ?></td>
+            </tr>
+            <tr>
+                <td>Bulan</td>
+                <td>:</td>
+                <td><?php echo $bulan ?></td>
+            </tr>
+            <tr>
+                <td>Tahun</td>
+                <td>:</td>
+                <td><?php echo $tahun ?></td>
+            </tr>
+        </table>
+
+        <table class="table table-striped table-bordered mt-3">
+            <tr>
+                <th class="text-center" width="5%">No</th>
+                <th class="text-center">Keterangan</th>
+                <th class="text-center">Jumlah</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Gaji Pokok</td>
+                <td>Rp. <?php echo number_format($ps->gaji_pokok, 0, ',', '.') ?></td>
+            </tr>
+
+            <tr>
+                <td>2</td>
+                <td>Tunjangan Transportasi</td>
+                <td>Rp. <?php echo number_format($ps->tj_transport, 0, ',', '.') ?></td>
+            </tr>
+
+            <tr>
+                <td>3</td>
+                <td>Uang Makan</td>
+                <td>Rp. <?php echo number_format($ps->uang_makan, 0, ',', '.') ?></td>
+            </tr>
+
+            <tr>
+                <td>4</td>
+                <td>Potongan</td>
+                <td>Rp.<?php echo number_format($total_potongan, 0, ',', '.') ?></td>
+            </tr>
+
+            <tr>
+                <th colspan="2" style="text-align: right;">Total Gaji</th>
+                <th>Rp.<?php echo number_format($total_gaji, 0, ',', '.') ?></th>
+            </tr>
+        </table>
+
+        <table width="100%">
+            <tr>
+                <td></td>
+                <td>
+                    <p>Pegawai</p>
+                    <br>
+                    <br>
+                    <p class="font-weight-bold"><?php echo $ps->nama_pegawai ?></p>
+                </td>
+                <br>
+                <td width="250px">
+                    <p>Pangkalpinang, <?php echo date("d M Y") ?> <br> Finance,</p>
+                    <br>
+                    <br>
+                    <p>___________________</p>
+                </td>
+            </tr>
+        </table>
+
+    <?php endforeach; ?>
+
+</body>
+
+</html>
+
+<script type="text/javascript">
+    window.print();
+</script>
